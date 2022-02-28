289c6c1d9c9f5cdd4460aa161cc0463d27a9beaf
Gonzalo Arechavala <gonzaloap@hotmail.com>
2022-02-28 20:05:12 +0100

    Se añade formulario de reserva y cambios en los estilos

--- src/pages/clientes/Reservas/Reservas.jsx
+++ src/pages/clientes/Reservas/Reservas.jsx
@@ -1,91 +1,186 @@
 import React, { useState } from "react";
-
+import "./Reservas.scss";
 
 const INITIAL_STATE = {
-    diners: "",
-    date: "",
-    hour: "",
-    name: "",
-    surnames: "",
-    phone: "",
-    email: "",
-    allergies:"",
-    request:"",
-    newsletter:"",
-  };
-
+  diners: "",
+  date: "",
+  hour: "",
+  name: "",
+  surnames: "",
+  phone: "",
+  email: "",
+  allergies: "",
+  request: "",
+  newsletter: "",
+};
 
 const Reservas = () => {
+  const [state, setState] = useState(INITIAL_STATE);
 
-    const [state, setState] = useState(INITIAL_STATE);
+  const submitForm = (ev) => {
+    ev.preventDefault();
+    console.log("Datos del formulario:");
+    console.log(state);
+  };
 
-    const submitForm = (ev) => {
-        ev.preventDefault();
-    };
+  const handleInput = (ev) => {
+    const { name, value } = ev.target;
+    setState({ ...state, [name]: value });
 
-    const handleInput = (ev) => {
-        const { name, value } = ev.target;
-        setState({ ...state, [name]: value });
-        console.log(state);
-    };
+  };
 
-    return (
+  return (
+    <div className="formulario">
+      <h1>Reservas</h1>
+
+      
+      {/* <button
+        className="btnreservas"
+        onClick={handleInput}
+        name="diners"
+        value="1"
+      >
+        1
+      </button>
+      <button
+        className="btnreservas"
+        onClick={handleInput}
+        name="diners"
+        value="2"
+      >
+        2
+      </button>
+      <button
+        className="btnreservas"
+        onClick={handleInput}
+        name="diners"
+        value="3"
+      >
+        3
+      </button>
+      <button
+        className="btnreservas"
+        onClick={handleInput}
+        name="diners"
+        value="4"
+      >
+        4
+      </button>
+       */}
+
+      <form onSubmit={submitForm}>
+
+      <p>Número de personas:</p>
+        <input label="1" type="radio" name="diners" value="1" onChange={handleInput} />
+        <input label="2" type="radio" name="diners" value="2" onChange={handleInput} />
+        <input label="3" type="radio" name="diners" value="3" onChange={handleInput} />
+        <input label="4" type="radio" name="diners" value="4" onChange={handleInput} />
+        <input label="5" type="radio" name="diners" value="5" onChange={handleInput} />
+        <input label="6" type="radio" name="diners" value="6" onChange={handleInput} />
+        <input label="7" type="radio" name="diners" value="7" onChange={handleInput} />
+        <input label="8" type="radio" name="diners" value="8" onChange={handleInput} />
+
+
+        <p>Seleccione una fecha:</p>
+        <label>
+          <input
+            type="date"
+            name="date"
+            onChange={handleInput}
+            placeholder="Fecha"
+          ></input>
+        </label>
+
+        <p>Seleccione una hora:</p>
+        <p className="hora">Comida</p>
+        <input label="13:30" type="radio" name="hour" value="13:30" onChange={handleInput} />
+        <input label="14:00" type="radio" name="hour" value="14:00" onChange={handleInput} />
+        <input label="14:30" type="radio" name="hour" value="14:30" onChange={handleInput} />
+        <input label="15:00" type="radio" name="hour" value="15:00" onChange={handleInput} />
+        <p className="hora">Cena:</p>
+        <input label="20:30" type="radio" name="hour" value="20:30" onChange={handleInput} />
+        <input label="21:00" type="radio" name="hour" value="21:00" onChange={handleInput} />
+        <input label="21:30" type="radio" name="hour" value="21:30" onChange={handleInput} />
+        <input label="22:00" type="radio" name="hour" value="22:00" onChange={handleInput} />
+
+        <p>Datos de la reserva:</p>
+        <label>
+          <input
+            type="text"
+            name="name"
+            value={state.name}
+            onChange={handleInput}
+            placeholder="Nombre *"
+          />
+        </label>
+        <label>
+          <input
+            type="text"
+            name="surnames"
+            value={state.surnames}
+            onChange={handleInput}
+            placeholder="Apellidos *"
+          />
+        </label>
+        <label>
+          <input
+            type="text"
+            name="phone"
+            value={state.phone}
+            onChange={handleInput}
+            placeholder="Telefono *"
+          />
+        </label>
+        <label>
+          <input
+            type="text"
+            name="email"
+            value={state.email}
+            onChange={handleInput}
+            placeholder="Email *"
+          />
+        </label>
+        <div className="vspace"></div>
+        <label>
+          <input
+            type="text"
+            name="allergies"
+            value={state.allergies}
+            onChange={handleInput}
+            placeholder="Alergias"
+          />
+        </label>
+
+        <label>
+          <textarea
+            type="text"
+            name="request"
+            value={state.request}
+            onChange={handleInput}
+            placeholder="Solicitud particular"
+          />
+        </label>
+        <div></div>
+        <label>
+          <input
+            type="checkbox"
+            name="newsletter"
+            value={state.newsletter}
+            onChange={handleInput}
+            placeholder="Newsletter"
+          />
+          Deseo recibir ofertas especiales y notificaciones del restaurante por
+          correo electrónico y mensajes de texto.
+        </label>
+
         <div>
-        Reservas
-        <form onSubmit={submitForm}>
-                <fieldset>
-                    <label>
-                        <p>Personas</p>
-                        <input type="text" name="diners" value={state.diners} onChange={handleInput} />
-                    </label>
-
-                    <label>
-                        <p>Fecha</p>
-                        <input type="text" name="date" value={state.date} onChange={handleInput} />
-                    </label>
-
-                    <label>
-                        <p>Hora</p>
-                        <input type="text" name="hour" value={state.hour} onChange={handleInput} />
-                    </label>
-
-                    <label>
-                        <p>Nombre</p>
-                        <input type="text" name="name" value={state.name} onChange={handleInput} />
-                    </label>
-                    <label>
-                        <p>Apellidos</p>
-                        <input type="text" name="surnames" value={state.surnames} onChange={handleInput} />
-                    </label>
-                    <label>
-                        <p>Telefono</p>
-                        <input type="text" name="phone" value={state.phone} onChange={handleInput} />
-                    </label>                                        
-                    <label>
-                        <p>Email</p>
-                        <input type="text" name="email" value={state.email} onChange={handleInput} />
-                    </label>
-                    <label>
-                        <p>Alergias</p>
-                        <input type="text" name="allergies" value={state.allergies} onChange={handleInput} />
-                    </label>        
-
-                    <label>
-                        <p>Solicitud particular</p>
-                        <input type="text" name="request" value={state.request} onChange={handleInput} />
-                    </label>    
-                    <label>
-                        <p>newsletter</p>
-                        <input type="text" name="newsletter" value={state.newsletter} onChange={handleInput} />
-                    </label>                                                        
-
-                    <div>
-                        <button type="submit">Guardar Perfil</button>
-                    </div>
-                </fieldset>
-            </form>
+          <button className="standardButton" type="submit">
+            Reservar
+          </button>
         </div>
-    );
-}
+      </form>
+    </div>
+  );
+};
 
 export default Reservas;
