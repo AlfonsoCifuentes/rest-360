52dc1d7a992c5c6c71160584f3ae6e08717f185d
Gonzalo Arechavala <gonzaloap@hotmail.com>
2022-03-01 00:07:39 +0100

    Formulario, confirmación de registro

--- src/pages/clientes/Reservas/Reservas.jsx
+++ src/pages/clientes/Reservas/Reservas.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import "./Reservas.scss";
 
 const INITIAL_STATE = {
@@ -12,29 +13,87 @@ const INITIAL_STATE = {
   allergies: "",
   request: "",
   newsletter: "",
+  privacy:"",
 };
 
 const Reservas = () => {
   const [state, setState] = useState(INITIAL_STATE);
+  const [envioForm, setEnvioForm] = useState(false);
 
   const submitForm = (ev) => {
     ev.preventDefault();
+
+      // Validamos campos del formulario.
+    let errors = {};
+
+    if (!state.diners) {
+      errors.diners = "Diners is required!";
+    }
+  
+    if (!state.date) {
+      errors.date = "Date is required!";
+    }
+  
+    if (!state.hour) {
+      errors.hour = "Hour is required!";
+    }
+  
+    if (!state.name) {
+      errors.name = "Name is required!";
+    } else if (state.name.length <= 1) {
+      errors.name = "Name has to be 1 character at less!";
+    }
+
+    if (!state.surnames) {
+      errors.surnames = "Surnames is required!";
+    } else if (state.name.length <= 1) {
+      errors.surnames = "Surnames has to be 1 character at less!";
+    }
+
+    if (!state.surnames) {
+      errors.surnames = "Surnames is required!";
+    } else if (state.name.length <= 1) {
+      errors.surnames = "Surnames has to be 1 character at less!";
+    }
+
+    if (!state.phone) {
+      errors.phone = "Phone is required!";
+    } else if (state.phone.length < 9) {
+      errors.phone = "Phone has to be 9 character at less!";
+    }
+
+    if (!state.privacy) {
+      errors.privacy = "The privacy is required!";
+    }
+
+
+    // if(!errors.length){
+    //   console.log(errors);
+    //   errors = {};
+    // }else{
+
+      console.log(errors);
+    // aqui MySQL para guardado de datos.
     console.log("Datos del formulario:");
     console.log(state);
+    setEnvioForm(true);
+    
+    // }
+    
   };
+
 
   const handleInput = (ev) => {
     const { name, value } = ev.target;
     setState({ ...state, [name]: value });
-
   };
 
-  return (
-    <div className="formulario">
-      <h1>Reservas</h1>
+  if (!envioForm) {
+    return (
+      <div className="divForm">
+        <h1>Reservas</h1>
 
-      
-      {/* <button
+        {/* <button
         className="btnreservas"
         onClick={handleInput}
         name="diners"
@@ -68,119 +127,246 @@ const Reservas = () => {
       </button>
        */}
 
-      <form onSubmit={submitForm}>
-
-      <p>Número de personas:</p>
-        <input label="1" type="radio" name="diners" value="1" onChange={handleInput} />
-        <input label="2" type="radio" name="diners" value="2" onChange={handleInput} />
-        <input label="3" type="radio" name="diners" value="3" onChange={handleInput} />
-        <input label="4" type="radio" name="diners" value="4" onChange={handleInput} />
-        <input label="5" type="radio" name="diners" value="5" onChange={handleInput} />
-        <input label="6" type="radio" name="diners" value="6" onChange={handleInput} />
-        <input label="7" type="radio" name="diners" value="7" onChange={handleInput} />
-        <input label="8" type="radio" name="diners" value="8" onChange={handleInput} />
-
-
-        <p>Seleccione una fecha:</p>
-        <label>
+        <form onSubmit={submitForm}>
+          <p>Número de personas:</p>
           <input
-            type="date"
-            name="date"
+            label="1"
+            type="radio"
+            name="diners"
+            value="1"
             onChange={handleInput}
-            placeholder="Fecha"
-          ></input>
-        </label>
+          />
+          <input
+            label="2"
+            type="radio"
+            name="diners"
+            value="2"
+            onChange={handleInput}
+          />
+          <input
+            label="3"
+            type="radio"
+            name="diners"
+            value="3"
+            onChange={handleInput}
+          />
+          <input
+            label="4"
+            type="radio"
+            name="diners"
+            value="4"
+            onChange={handleInput}
+          />
+          <input
+            label="5"
+            type="radio"
+            name="diners"
+            value="5"
+            onChange={handleInput}
+          />
+          <input
+            label="6"
+            type="radio"
+            name="diners"
+            value="6"
+            onChange={handleInput}
+          />
+          <input
+            label="7"
+            type="radio"
+            name="diners"
+            value="7"
+            onChange={handleInput}
+          />
+          <input
+            label="8"
+            type="radio"
+            name="diners"
+            value="8"
+            onChange={handleInput}
+          />
 
-        <p>Seleccione una hora:</p>
-        <p className="hora">Comida</p>
-        <input label="13:30" type="radio" name="hour" value="13:30" onChange={handleInput} />
-        <input label="14:00" type="radio" name="hour" value="14:00" onChange={handleInput} />
-        <input label="14:30" type="radio" name="hour" value="14:30" onChange={handleInput} />
-        <input label="15:00" type="radio" name="hour" value="15:00" onChange={handleInput} />
-        <p className="hora">Cena:</p>
-        <input label="20:30" type="radio" name="hour" value="20:30" onChange={handleInput} />
-        <input label="21:00" type="radio" name="hour" value="21:00" onChange={handleInput} />
-        <input label="21:30" type="radio" name="hour" value="21:30" onChange={handleInput} />
-        <input label="22:00" type="radio" name="hour" value="22:00" onChange={handleInput} />
+          <p>Seleccione una fecha:</p>
+          <label>
+            <input
+              type="date"
+              name="date"
+              onChange={handleInput}
+              placeholder="Fecha"
+            ></input>
+          </label>
 
-        <p>Datos de la reserva:</p>
-        <label>
+          <p>Seleccione una hora:</p>
+          <p className="hora">Comida</p>
           <input
-            type="text"
-            name="name"
-            value={state.name}
+            label="13:30"
+            type="radio"
+            name="hour"
+            value="13:30"
             onChange={handleInput}
-            placeholder="Nombre *"
           />
-        </label>
-        <label>
           <input
-            type="text"
-            name="surnames"
-            value={state.surnames}
+            label="14:00"
+            type="radio"
+            name="hour"
+            value="14:00"
             onChange={handleInput}
-            placeholder="Apellidos *"
           />
-        </label>
-        <label>
           <input
-            type="text"
-            name="phone"
-            value={state.phone}
+            label="14:30"
+            type="radio"
+            name="hour"
+            value="14:30"
             onChange={handleInput}
-            placeholder="Telefono *"
           />
-        </label>
-        <label>
           <input
-            type="text"
-            name="email"
-            value={state.email}
+            label="15:00"
+            type="radio"
+            name="hour"
+            value="15:00"
             onChange={handleInput}
-            placeholder="Email *"
           />
-        </label>
-        <div className="vspace"></div>
-        <label>
+          <p className="hora">Cena:</p>
           <input
-            type="text"
-            name="allergies"
-            value={state.allergies}
+            label="20:30"
+            type="radio"
+            name="hour"
+            value="20:30"
             onChange={handleInput}
-            placeholder="Alergias"
           />
-        </label>
+          <input
+            label="21:00"
+            type="radio"
+            name="hour"
+            value="21:00"
+            onChange={handleInput}
+          />
+          <input
+            label="21:30"
+            type="radio"
+            name="hour"
+            value="21:30"
+            onChange={handleInput}
+          />
+          <input
+            label="22:00"
+            type="radio"
+            name="hour"
+            value="22:00"
+            onChange={handleInput}
+          />
 
-        <label>
-          <textarea
-            type="text"
-            name="request"
-            value={state.request}
-            onChange={handleInput}
-            placeholder="Solicitud particular"
-          />
-        </label>
-        <div></div>
-        <label>
-          <input
-            type="checkbox"
-            name="newsletter"
-            value={state.newsletter}
-            onChange={handleInput}
-            placeholder="Newsletter"
-          />
-          Deseo recibir ofertas especiales y notificaciones del restaurante por
-          correo electrónico y mensajes de texto.
-        </label>
+          <p>Datos de la reserva:</p>
+          <label>
+            <input
+              type="text"
+              name="name"
+              value={state.name}
+              onChange={handleInput}
+              placeholder="Nombre *"
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="surnames"
+              value={state.surnames}
+              onChange={handleInput}
+              placeholder="Apellidos *"
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="phone"
+              value={state.phone}
+              onChange={handleInput}
+              placeholder="Telefono *"
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="email"
+              value={state.email}
+              onChange={handleInput}
+              placeholder="Email"
+            />
+          </label>
+          <div className="vSpace"></div>
+          <label>
+            <input
+              type="text"
+              name="allergies"
+              value={state.allergies}
+              onChange={handleInput}
+              placeholder="Alergias"
+            />
+          </label>
 
-        <div>
-          <button className="standardButton" type="submit">
-            Reservar
-          </button>
+          <label>
+            <textarea
+              type="text"
+              name="request"
+              value={state.request}
+              onChange={handleInput}
+              placeholder="Solicitud particular"
+            />
+          </label>
+          <div></div>
+          <label>
+            <input
+              type="checkbox"
+              name="newsletter"
+              checked={state.newsletter}
+              onChange={handleInput}
+            />
+            Deseo recibir ofertas especiales y notificaciones del restaurante
+            por correo electrónico y mensajes de texto.
+          </label>
+          <div className="vSpace"></div>
+          <label>
+            <input
+              type="checkbox"
+              name="privacy"
+              checked={state.privacy}
+              onChange={handleInput}
+            />
+            Acepto Políticas de Privacidad.
+          </label>
+
+          <div>
+            <button className="standardButton" type="submit">
+              Reservar
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="divForm">
+        <h1>Reservas</h1>
+        <div className="confirmation">
+          <img
+            className="bigIcon"
+            src={require("../../../images/icons/RoundedTickIcon.png")}
+            alt="Facebook logo"
+          />
+          <div className="vSpace" />
+          Tu reserva se ha registrado con éxito.
+          <div className="vSpace" />
+          Día: {state.date}
+          <br />
+          Hora: {state.hour}
+          <br />
+          Personas: {state.diners} <br />
+          <div className="vSpace" />
+          ¡Te esperamos!
         </div>
-      </form>
-    </div>
-  );
+      </div>
+    );
+  }
 };
 
 export default Reservas;
