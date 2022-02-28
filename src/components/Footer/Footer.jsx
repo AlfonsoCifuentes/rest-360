e61145ce372dd45fac63fa3dfcd3e9e4f1016f9b
Alfonso Cifuentes <alfonsocifuentes83@gmail.com>
2022-02-28 02:09:53 +0100

    Añadido footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,45 @@
 import React from 'react'
+import "./styles.scss"
 
 const Footer = () => {
   return (
     <div className='footer'>
 
-        <div className='footer_white'>
+        <div className='footer__white'>
+
+            <div>
+                <img
+                className="footer__white--logo"
+                src={require ("../../images/icons/Logo360Negro.png")}
+                alt="Logo"
+                />
+            </div>
+
+            <div className='footer__white__ubication'>
+                    <p>Gran Vía, 36 / 28002 Madrid <span><img src={require ("../../images/icons/positionIcon.png")} alt="Ubication icon" className='smallIcon'/></span></p>
+            </div>
+            
 
         </div>
 
         <div className='footer__yellow'>
-            <div>
-                <div>
-                    Logos
+            <div className='footer__yellow__up'>
+                <div className='footer__yellow__up__icons'>
+                    <img className='footer__yellow__up__icons--icon' src={require ("../../images/icons/FacebookIcon.png")} alt="Facebook logo"/>
+                    <img className='footer__yellow__up__icons--icon' src={require ("../../images/icons/WhastsappIcon.png")} alt="Whatsapp logo"/>
+                    <img className='footer__yellow__up__icons--icon' src={require ("../../images/icons/InstagramIcon.png")} alt="Instagram logo"/>     
                 </div>
                 <div>
-                    Reloj
+                    <img className='footer__yellow__up__clockIcon' src={require ("../../images/icons/ClockIcon.png")} alt="Clock icon"/>   
                 </div>
-                <div>
+                <div className='footer__yellow__up__text'>
                     <p>Aviso legal</p>
                     <p>Política de privacidad</p>
                     <p>Política de cookies</p>
                 </div>
             </div>
 
-            <div>
+            <div className='footer__yellow__down'>
                 <p>rest360º © Todos los derechos reservados</p>
             </div>
         </div>
