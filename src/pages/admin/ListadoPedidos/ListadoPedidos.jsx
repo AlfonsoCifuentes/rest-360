787ae27ab4a0a0b24ed7f1a4433ed197f41d9474
Gonzalo Arechavala <gonzaloap@hotmail.com>
2022-03-12 00:29:49 +0100

    Añadido listado de pedidos

--- src/pages/admin/ListadoPedidos/ListadoPedidos.jsx
+++ src/pages/admin/ListadoPedidos/ListadoPedidos.jsx
@@ -1,20 +1,61 @@
-import React, {useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
 
 const ListadoPedidos = () => {
+  //Props para el componente Acordeón:
 
-return (
+  //Carta completa
+  const [datosArray, setDatosArray] = useState([]);
+  //const [ticketsArray, setTicketsArray] = useState([]);
+
+  //Fetch de la carta al servidor
+  useEffect(() => {
+    fetch("http://localhost:3001/vga/orders")
+      .then((response) => response.json())
+      .then((data) => setDatosArray(data));
+  }, []);
+
+  console.log(setDatosArray);
+
+  return (
     <div className="mainDiv">
       <h1 className="title">Listado de Pedidos</h1>
       <div className="sectionParagraph">
-        <p>
-          La cocina de rest360º está basada en los productos de la tierra, en
-          elaboraciones especiadas, de sabores intensos; una cocina repleta de
-          excelencias.
-        </p>
-        
-      </div>
+        <table className="rwd-table">
+          <thead>
+            <tr>
+              <th>Pedido</th>
+              <th>Mesa</th>
+              <th>Usuario</th>
+              <th>Comensales</th>
+              <th>Importe</th>
+              <th>IVA</th>
+              <th>P.V.P.</th>
+              <th>Estado</th>
+              <th>Dia / Hora</th>
+              <th>Ver detalle</th>
+            </tr>
+          </thead>
+          <tbody>
+            {datosArray.map((item) => (
+              <tr key={item.id}>
+                <td className="div__table__center">{item.idOrder}</td>
+                <td className="div__table__center">{item.idDestino}</td>
+                <td className="div__table__center">{item.idUser}</td>
+                <td className="div__table__center">{item.numPerson}</td>
+                <td className="div__table__right">{item.costNeto}</td>
+                <td className="div__table__right">{item.iva}</td>
+                <td className="div__table__right">{item.pvp}</td>
+                <td className="div__table__right">{item.estado}</td>
+                <td className="div__table__right">{item.day}</td>
+                <td data-th="Ver detalle:" className="div__table__center"><img className="controlIcon" src={require("../../../images/icons/flechaIcon.png")} alt="Añadir al pedido" /></td>
+              </tr>
+            ))}
 
+
+
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
